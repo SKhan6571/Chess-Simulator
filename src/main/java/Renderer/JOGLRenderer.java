fbcf3caa289dcecea86cd84a82c720ccf96827f9
Ryan-Mosier <ryan.m.mosier@gmail.com>
2025-11-26 11:15:05 -0700

    Refactor `JOGLRenderer` implementation. Split `render()` logic into `update()` and `display()`, add buffer clearing, and improve code readability for initialization and methods.

--- src/main/java/Renderer/JOGLRenderer.java
+++ src/main/java/Renderer/JOGLRenderer.java
@@ -4,9 +4,10 @@ import Board.Board;
 
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
+
 import javax.swing.*;
 
-public class JOGLRenderer implements Renderer{
+public class JOGLRenderer implements Renderer{ // this also needs to implement GLEventListener, which means that most of this is broken right now...
     //OpenGL renderer, OGL is state-based, so this might be a bit of a mess.
     private Board board;
     private boolean initialized = false;
@@ -18,38 +19,46 @@ public class JOGLRenderer implements Renderer{
     private GLCapabilities capabilities = null;
     private GLCanvas canvas = null;
 
-    public JOGLRenderer(GLCanvas canvas){
+    public JOGLRenderer() {
         this.init();
     }
 
-    private void init(){
-        if(initialized) return;
-        this.drawable = (GLAutoDrawable) this;
+    private void init() {
+        if (initialized) return;
+        this.drawable = (GLAutoDrawable) this; // this is probably broken...
         this.gl = drawable.getGL().getGL4();
         this.profile = GLProfile.get(GLProfile.GL4);
         this.capabilities = new GLCapabilities(profile);
-        this.canvas =  new GLCanvas(capabilities);
+        this.canvas = new GLCanvas(capabilities);
         initialized = true;
     }
 
     @Override
     public void render() {
+        update();
+        display();
+    }
 
+    @Override
+    public void display() {
+        // display to screen, (just a buffer swap?)
     }
 
     @Override
     public void update() {
-
+        // prepare the next frame
+        clear();
     }
 
 
     @Override
     public void clear() {
-        gl.glClear( GL4.GL_COLOR_BUFFER_BIT);
+        // clears the next buffer
+        gl.glClear(GL4.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
-    public void setBoard(Board board){
+    public void setBoard(Board board) {
 
     }
 }
--- src/main/java/Renderer/Renderer.java
+++ src/main/java/Renderer/Renderer.java
@@ -2,7 +2,7 @@ package Renderer;
 
 import Board.Board;
 
-public interface Renderer {
+public interface Renderer{
     // The renderer will assume that the board is located at a constant location in memory, set by 'setBoard()'.
     // The update() and display() methods will be called every frame.
     // render() wraps both for convenience. But they are provided separately for flexibility.
