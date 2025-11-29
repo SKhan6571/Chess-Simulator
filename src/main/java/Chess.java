7460015369fde8228a9a30c788218890fbfc069c
joelthenry <joelthenry@gmail.com>
2025-11-29 14:10:42 -0700

    added input intake and parsing and ask the user which piece theyd like to use

--- src/main/java/Chess.java
+++ src/main/java/Chess.java
@@ -1,10 +1,32 @@
 import board.Board;
 import renderer.BoardPrinter;
+import java.util.Scanner;
+import input.textInputParser;
 
 public class Chess {
     public static void main(String[] args) {
         ///simple ascii board print test
+        ////////USES DEPENDENCY INJECTION!!!!!!!!!!!!!!!
         Board board = new Board();
         BoardPrinter.printBoard(board);
+
+
+        //GEt move input from user
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the piece to move (e.g., e2): ");
+        String rawInput = scanner.nextLine();
+        int[] pieceLocation = textInputParser.parseInput(rawInput);
+
+
+
+
+
+        //close scanner to prevent errors
+        scanner.close();
     }
+
+
+
+
+    
 }
